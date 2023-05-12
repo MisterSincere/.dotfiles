7b3acf9d639780984971768b595532a1830f256b
Nikkel Heesen <n.heesen@tu-bs.de>
2023-05-12 09:16:19 +0200

    alternatives also not working concerning no bg coloring

--- nvim/.config/nvim/lua/coloring.lua
+++ nvim/.config/nvim/lua/coloring.lua
@@ -6,3 +6,5 @@ vim.cmd('syntax on')
 
 --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--vim.cmd('hi! Normal guibg=NONE gui=NONE ctermbg=NONE cterm=NONE')
+--vim.cmd('hi! SignColumn guibg=NONE gui=NONE ctermbg=NONE cterm=NONE')
