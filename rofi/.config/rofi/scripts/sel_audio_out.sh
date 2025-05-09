afe615927701af81165e000cf9df45d3504ab677
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2025-05-09 13:55:26 +0200

    timer script, hyprlock

--- rofi/.config/rofi/scripts/sel_audio_out.sh
+++ rofi/.config/rofi/scripts/sel_audio_out.sh
@@ -2,7 +2,11 @@
 
 dir="$HOME/.config/rofi/styles"
 
-options=$(pamixer --list-sinks | awk -F " \"" '{ if (NR!=1) { print substr($4, 1, length($4)-1) } }' | grep -iv hdmi)
+# filter audio outputs down to readable names
+# @note don't show hdmi outputs
+all_options=$(pamixer --list-sinks)
+options=$(echo -e "${all_options}" | awk -F " \"" '{ if (NR!=1) { print substr($4, 1, length($4)-1) } }' | grep -iv hdmi)
 
+# give options to rofi
 selection=$(echo -e "${options}" | rofi -dmenu -i -theme "$dir/audio_in_dialog.rasi")
-coproc ( pamixer sink $selection )
+coproc ( pamixer --sink $selection )
