bac1cf92ae55684a5b15d693d275d831b7370529
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2025-05-13 14:36:25 +0200

    hyprland media keys and show volume noti script for dunst updated

--- hyprland/.config/hypr/scripts/show_volume_noti.sh
+++ hyprland/.config/hypr/scripts/show_volume_noti.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-pamixer "$@"
-
 msg_tag="volumebar"
-volume=$(pamixer --get-volume)
-mute=$(pamixer --get-mute)
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F " " '{ print $2 * 100 }')
+mute=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F " " '{ print $3 }')
 
-if [[ $mute == "true" ]]; then
+if [[ $mute == "[MUTED]" ]]; then
   dunstify -a "change_volume" -u low -h string:x-dunst-stack-tag:$msg_tag "Volume muted"
 else
   dunstify -a "change_volume" -u low -h string:x-dunst-stack-tag:$msg_tag -h int:value:"$volume" "Volume: ${volume}%"
 fi
 
-mpv /usr/share/sounds/freedesktop/stereo/message.oga &> /dev/null
-
+mpv /usr/share/sounds/freedesktop/stereo/message.oga
