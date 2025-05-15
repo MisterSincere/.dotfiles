7949ae7aac4bf1632d5f27f4697998b63705b456
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2025-05-15 15:27:34 +0200

    waybar changes

--- rofi/.config/rofi/scripts/sel_audio_out.sh
+++ rofi/.config/rofi/scripts/sel_audio_out.sh
@@ -4,6 +4,7 @@ dir="$HOME/.config/rofi/styles"
 
 # filter audio outputs down to readable names
 # @note don't show hdmi outputs
+all_options=$(wpctl status)
 all_options=$(pamixer --list-sinks)
 options=$(echo -e "${all_options}" | awk -F " \"" '{ if (NR!=1) { print substr($4, 1, length($4)-1) } }' | grep -iv hdmi)
 
