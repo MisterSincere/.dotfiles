0c1e34b84692f5c4c67852ed7a1327c07dc99697
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2025-09-01 14:43:14 +0200

    removed weird duplicate fish thingies

--- fish_l/fish/.config/fish/functions/cg.fish
+++
@@ -1,3 +0,0 @@
-function cg --wraps=./.config/scripts/cg_helper_script.sh --description 'alias cg=./.config/scripts/cg_helper_script.sh'
-  ~/.config/scripts/cg_helper_script.sh $argv; 
-end
--- fish_l/fish/.config/fish/functions/mtpull.fish
+++
@@ -1,3 +0,0 @@
-function mtpull --wraps='bash ~/.config/scripts/mtstudio_pull.sh' --description 'alias mtpull bash ~/.config/scripts/mtstudio_pull.sh'
-  bash ~/.config/scripts/mtstudio_pull.sh $argv; 
-end
