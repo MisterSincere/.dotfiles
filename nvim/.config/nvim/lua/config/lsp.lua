1b24e982f6c269dc19b3bde30889ac5018220301
Nikkel Heesen <heesen.nikkel@madnessstudio.de>
2025-09-24 12:07:18 +0200

    nvim: lsp keymaps commented

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,20 +1,20 @@
 -- config/lsp.lua
 
-local util = require('lspconfig.util')
+local map = require("utils.keymap")
 
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
 	local opts = {buffer = event.buf}
-	vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	vim.keymap.set('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-	vim.keymap.set('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+--	map.n('K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--	map.n('gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--	map.n('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+--	map.n('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--	map.n('gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+--	map.n('gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--	map.n('gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+--	map.n('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+--	map.n(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     end
 })
 
